cb46252ffa98a244efb8f84c174d648bed71eae5
Reetik Patel <reetikp@gmail.com>
2025-11-24 19:46:22 -0600

    🚧 new section

---
+++ src/components/IntroSection.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="min-h-screen bg-(--black) text-(--cream) p-8">
+    <div class="max-w-4xl mx-auto">
+      <h2 class="text-4xl mb-6">New Section</h2>
+      <p class="text-lg">Content goes here.</p>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import ScrollIndicator from '@/components/ScrollIndicator.vue'
+import IntroSection from '@/components/IntroSection.vue'
 </script>
 
 <template>
-  <div class="relative">
-    <img src="@/assets/intro.gif" alt="Intro" class="w-screen h-screen object-cover" />
-    <ScrollIndicator />
+  <div>
+    <div class="relative">
+      <img src="@/assets/intro.gif" alt="Intro" class="w-screen h-screen object-cover" />
+      <ScrollIndicator />
+    </div>
+
+    <IntroSection />
   </div>
 </template>
