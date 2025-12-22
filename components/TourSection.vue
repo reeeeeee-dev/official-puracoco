97af6315bacdb5821952207391188fe427471e2f
Reetik Patel <reetikp@gmail.com>
2025-12-21 21:23:13 -0600

    ✨ tour section

--- components/TourSection.vue
+++ components/TourSection.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-const { data, error, status } = await useFetch('/api/events', {
+import { ArrowRightIcon } from 'lucide-vue-next'
+import type { BandsInTownEvent } from '~/types/bandsintown'
+
+const { data, error, status } = await useFetch<BandsInTownEvent[]>('/api/events', {
   server: true,
 })
 </script>
@@ -16,8 +19,44 @@ const { data, error, status } = await useFetch('/api/events', {
           There was an error fetching the events. Please refresh the page or try again later.
         </div>
         <div v-else-if="status === 'pending'">Loading ...</div>
-        <div v-else-if="status === 'success'">
-          {{ JSON.stringify(data) }}
+        <div v-else-if="status === 'success'" class="flex flex-col gap-4">
+          <NuxtLink
+            v-for="event in data?.slice(0, 5)"
+            :key="event.id"
+            :to="`/events/${event.id}`"
+            class="bg-(--green) text-(--cream) p-4 rounded-md flex gap-2 justify-between items-center hover:bg-(--red) cursor-pointer transition-colors duration-300"
+          >
+            <div class="flex gap-6">
+              <div class="flex flex-col items-center text-(--cream) font-bold">
+                <div class="text-xl">
+                  {{ new Date(event.datetime).toLocaleDateString('en-US', { month: 'short' }) }}
+                </div>
+                <div class="text-2xl">
+                  {{ new Date(event.datetime).getDate() }}
+                </div>
+                <div class="text-lg">
+                  {{
+                    new Date(event.datetime).toLocaleTimeString('en-US', {
+                      hour: 'numeric',
+                      minute: '2-digit',
+                    })
+                  }}
+                </div>
+              </div>
+              <div class="flex flex-col gap-2">
+                <div class="flex flex-col">
+                  <div class="text-xl font-bold text-(--black)">
+                    {{ event.venue.city }}
+                  </div>
+                  <div class="text-sm text-(--black)">
+                    {{ event.venue.region }}, {{ event.venue.country }}
+                  </div>
+                </div>
+                <div class="text-lg text-(--black)">{{ event.venue.name }}</div>
+              </div>
+            </div>
+            <ArrowRightIcon class="size-6" />
+          </NuxtLink>
         </div>
       </div>
     </div>
