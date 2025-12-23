798d2e6e46a7ce0ac49b35c997a100824f4848bb
Reetik Patel <reetikp@gmail.com>
2025-12-23 15:45:39 -0600

    🎨 tour section cleanup

--- components/TourSection.vue
+++ components/TourSection.vue
@@ -7,7 +7,7 @@ const { data, error, status } = await useFetch<BandsInTownEvent[]>('/api/events'
 })
 </script>
 <template>
-  <div class="relative h-[calc(200vh-4rem)] bg-(--black) text-(--cream) z-0 snap-start snap-always">
+  <div class="relative h-dvh bg-(--black) text-(--cream) z-0 snap-start snap-always">
     <div
       class="sticky snap-align-none top-16 w-full h-[calc(100vh-4rem)] flex items-center justify-center gap-48"
     >
@@ -17,7 +17,6 @@ const { data, error, status } = await useFetch<BandsInTownEvent[]>('/api/events'
       <div class="flex flex-col gap-8">
         <div v-if="error" class="text-base max-w-prose">
           There was an error fetching the events. Please refresh the page or try again later.
-          {{ JSON.stringify(error) }}
         </div>
         <div v-else-if="status === 'pending'">Loading ...</div>
         <div v-else-if="status === 'success'" class="flex flex-col gap-4">
