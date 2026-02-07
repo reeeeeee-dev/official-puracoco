f4538f1f101bb9cd5ce254e889a08fc8d7525f71
Reetik Patel <reetikp@gmail.com>
2026-02-07 12:58:39 -0600

    🎨 applied single root node in app

--- app.vue
+++ app.vue
@@ -7,23 +7,25 @@ const route = useRoute()
 </script>
 
 <template>
-  <LoadingScreen />
-  <NavbarHeader />
+  <main>
+    <LoadingScreen />
+    <NavbarHeader />
 
-  <!-- Hidden iframe to preload newsletter form -->
-  <ClientOnly>
-    <iframe
-      src="https://bandsintown.com/artist/15542693/email_signup_form?headerTextColor=rgba(255,207,153,1)&backgroundColor=rgba(12,15,10,1)&ctaBackgroundColor=rgba(242,67,51,1)&title=MAILING LIST&headerTextStyle=normal&headerText=Sign up to get the latest updates&font=Poppins&ctaIcon=show&ctaBorderRadius=4px&ctaBorderWidth=2px&ctaBorderColor=rgba(12,15,10,1)&ctaFontColor=rgba(255,207,153,1)&alignment=center&emailInputField=show&ctaLabel=Subscribe&layout=narrow&locale=en&ctaSize=small&affilCode=&appId=f37e48c6eccbf2f3b16b571955266fd0"
-      class="fixed -left-[9999px] -top-[9999px] w-1 h-1 opacity-0 pointer-events-none"
-      aria-hidden="true"
-      tabindex="-1"
-    />
-  </ClientOnly>
+    <!-- Hidden iframe to preload newsletter form -->
+    <ClientOnly>
+      <iframe
+        src="https://bandsintown.com/artist/15542693/email_signup_form?headerTextColor=rgba(255,207,153,1)&backgroundColor=rgba(12,15,10,1)&ctaBackgroundColor=rgba(242,67,51,1)&title=MAILING LIST&headerTextStyle=normal&headerText=Sign up to get the latest updates&font=Poppins&ctaIcon=show&ctaBorderRadius=4px&ctaBorderWidth=2px&ctaBorderColor=rgba(12,15,10,1)&ctaFontColor=rgba(255,207,153,1)&alignment=center&emailInputField=show&ctaLabel=Subscribe&layout=narrow&locale=en&ctaSize=small&affilCode=&appId=f37e48c6eccbf2f3b16b571955266fd0"
+        class="fixed -left-[9999px] -top-[9999px] w-1 h-1 opacity-0 pointer-events-none"
+        aria-hidden="true"
+        tabindex="-1"
+      />
+    </ClientOnly>
 
-  <div :class="route.path !== '/' ? 'flex flex-col min-h-screen' : ''">
-    <NuxtPage :class="route.path !== '/' ? 'mt-16 grow' : ''" />
-    <FooterSection v-if="route.path !== '/'" />
-  </div>
+    <div :class="route.path !== '/' ? 'flex flex-col min-h-screen' : ''">
+      <NuxtPage :class="route.path !== '/' ? 'mt-16 grow' : ''" />
+      <FooterSection v-if="route.path !== '/'" />
+    </div>
+  </main>
 </template>
 
 <style>
