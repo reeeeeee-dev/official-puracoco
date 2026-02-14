fd7f8bce06a72dce9ff2dcdcaf847c1a0c3d5fa4
Reetik Patel <reetikp@gmail.com>
2026-02-13 19:10:58 -0600

    ✨ new about section

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { ArrowRightIcon } from 'lucide-vue-next'
+import fullBodyImage from '~/assets/full_body.jpg'
 </script>
 
 <template>
-  <div class="relative h-screen snap-start snap-always">
-    <div class="h-full flex items-center justify-center">
+  <div class="relative h-screen snap-start snap-always overflow-hidden">
+    <div class="h-full flex items-center justify-between">
       <div
-        class="bg-(--black) text-(--cream) max-w-prose p-16 rounded-lg text-3xl items-center flex flex-col gap-4"
+        class="bg-(--black) text-(--cream) max-w-prose p-16 rounded-lg text-xl items-center flex flex-col gap-4 w-1/2"
       >
         <p>
           Pura Coco is an electrifying Alternative R&B singer/songwriter hailing from Northwest
@@ -22,12 +22,10 @@ import { ArrowRightIcon } from 'lucide-vue-next'
           tells a story of identity, passion, and authenticity.
         </p>
         <br />
-        <NuxtLink
-          to="/about"
-          class="m-auto bg-(--red) text-(--cream) p-4 rounded-lg flex items-center gap-2 hover:bg-(--green) transition-colors cursor-pointer"
-        >
-          About Me <ArrowRightIcon :size="24" />
-        </NuxtLink>
+      </div>
+
+      <div class="w-1/2">
+        <img :src="fullBodyImage" alt="Pura Coco" class="w-full h-full object-contain object-top" />
       </div>
     </div>
   </div>
